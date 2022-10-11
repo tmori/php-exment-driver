c3114b9ddca447d034c8047700ae68fc2387d998
Takashi Mori <kanetugu2015@gmail.com>
2022-10-12 08:28:10 +0900

    add update script

---
+++ php/driver/test/data/data_update_for_test.php
@@ -0,0 +1,82 @@
+
+<?php
+require 'vendor/autoload.php';
+
+use Pimple\Container;
+use ExmentApi\Driver\Driver;
+
+//base parameters
+$table_name = 'test_data';
+$col_num = 18;
+//TODO check why max=256 causes error response...
+$col_str_max = 255;
+$date = '2022-10-12';
+$user_id_start = 2;
+$repeat_num = 50;
+
+if( $argc != 3 ){
+    echo "Usage: " . $argv[0] . " <id1> <id2>\n";
+    exit(1);
+}
+$id1 = $argv[1];
+$id2 = $argv[2];
+
+$row_id = ((((int)$id1) - $user_id_start) * $repeat_num) + ((int)$id2);
+
+$container = new \Pimple\Container([
+    'driver' => [
+        'scheme' => 'http',
+        'url' => getenv('EXMENT_URL') . '/admin',
+    ],
+    'auth' => [
+        'grant_type' => 'api_key',
+        'client_id' => getenv('EXMENT_CLIENT_ID'),
+        'client_secret' => getenv('EXMENT_CLIENT_SECRET'),
+        'api_key' => getenv('EXMENT_APK_KEY'),
+        'scope' => 'value_write'
+    ],
+    'guzzle' => [
+        'verify' => false
+    ]
+]);
+$driver = new Driver($container);
+$result = $driver->api_key_authenticate();
+$code = strval($result->getStatusCode());
+$phrase = $result->getReasonPhrase();
+echo "code=${code} : ${phrase}\n";
+
+$update_data = [
+    'user' => $id1,
+    "date" => $date
+];
+for ( $i = 1; $i <= $col_num; $i++ )
+{
+    $col_name = 'col' . strval($i);
+    $value1 = 'col=' . str_pad(strval($i), 3, 0, STR_PAD_LEFT) . ':'; 
+    $value2 = 'usr=' . str_pad($id1, 3, 0, STR_PAD_LEFT) . ':'; 
+    $value3 = 'rid=' . str_pad($id2, 3, 0, STR_PAD_LEFT) . ':'; 
+    //$value4 = '';
+    $value4 = str_pad(strval($i), $col_str_max - strlen($value1 . $value2 . $value3), 'X', STR_PAD_LEFT); 
+    $value = $value1 . $value2 . $value3 . $value4;
+    $update_data[$col_name] = $value;
+}
+echo "row_id = " . $row_id . "\n";
+
+$res = $driver->getDataModel()->data_update(
+    $table=$table_name, 
+    $id = $row_id,
+    $value=$update_data,
+    $findKeys=NULL, 
+    $parent_type=NULL, 
+    $parent_id=NULL, 
+    $label=NULL);
+if ($res->getStatusCode() == "200")
+{
+    echo "OK: ResCode=" . $res->getStatusCode() . "\n";
+}
+else{
+    echo "NG: ResCode=" . $res->getStatusCode() . "\n";
+    //echo $res->getBody() . "\n";
+}
+
+?>
